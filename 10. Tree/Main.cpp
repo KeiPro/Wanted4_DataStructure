7f6b9532d3a677ebecc8281f3f97bdf043c485b4
keipro <keiprogramo@gmail.com>
2026-02-20 16:00:43 +0900

    다중 트리 구현

--- 10. Tree/Main.cpp
+++ 10. Tree/Main.cpp
@@ -2,13 +2,15 @@
 
 int main()
 {
-	Node<int>* parent = new Node<int>(10);
-	Node<int>* child = new Node<int>(5);
-	parent->AddChild(child);
-	//child->SetParent(parent);
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 
-	parent->RemoveChild(child);
-	/*Tree<int> tree(10);
+	//Node<int>* parent = new Node<int>(10);
+	//Node<int>* child = new Node<int>(5);
+	//parent->AddChild(child);
+	////child->SetParent(parent);
+
+	//parent->RemoveChild(child);
+	Tree<int> tree(10);
 	tree.AddChild(10, 9);
 	tree.AddChild(10, 8);
 	tree.AddChild(9, 7);
@@ -17,6 +19,15 @@ int main()
 	Node<int>* outNode = nullptr;
 	if (tree.Find(6, outNode))
 	{
+		
+	}
 
-	}*/
+	// 삭제 테스트.
+	if (tree.Remove(9))
+	{
+
+	}
+
+	// 전위 순회 테스트
+	tree.PreorderTraversal();
 }
